7a3ec0824605c6511b561cabaedc6888c8498443
ShunjiroOsada <shunjiroosada@gmail.com>
2018-11-09 04:08:55 +0900

    "my first commit"

---
+++ init.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wombat)))
+ '(font-use-system-font t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tool-bar-position (quote bottom)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 128 :width normal)))))
+(global-linum-mode t)
+(add-to-list 'auto-mode-alist'("\\.md\\'" . markdown-mode))
+(setq inhibit-startup-screen t)
