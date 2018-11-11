a11647d0cf557fbf0c83d346a9db53bc2775cfdd
ShunjiroOsada <shunjiroosada@gmail.com>
2018-11-12 01:17:12 +0900

    my second commit

--- init.el
+++ init.el
@@ -21,3 +21,52 @@
 (global-linum-mode t)
 (add-to-list 'auto-mode-alist'("\\.md\\'" . markdown-mode))
 (setq inhibit-startup-screen t)
+
+;; showing full-pass on title
+(setq frame-title-format "%f")
+
+;; deleting initial-scratch-message
+(setq initial-scratch-message "")
+
+;; active window move
+(global-set-key (kbd "<C-left>")  'windmove-left)
+(global-set-key (kbd "<C-down>")  'windmove-down)
+(global-set-key (kbd "<C-up>")    'windmove-up)
+(global-set-key (kbd "<C-right>") 'windmove-right)
+
+;; comment-out
+;; selected region
+(global-set-key (kbd "C-c o") 'comment-region)
+
+;;release-comment-out
+(global-set-key (kbd "C-c ;") 'comment-or-uncomment-region)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 5)))
+
+(setq completion-ignore-case t)
+
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "dark slate gray"))
+    (((class color)
+      (background light))
+     (:background  "#98FB98"))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+(global-hl-line-mode)
+
+(require 'wdired)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+(blink-cursor-mode 0)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq default-coding-systems 'utf-8)(set-terminal-coding-system 'euc-jp)
+(set-keyboard-coding-system 'euc-jp)
