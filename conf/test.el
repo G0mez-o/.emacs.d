253c748376e28cf0e12dbfb82b949deed9b45c2f
ShunjiroOsada <shunjiroosada@gmail.com>
2020-05-29 23:54:41 +0900

    delete unnecessary codes

--- conf/test.el
+++ conf/test.el
@@ -2,10 +2,7 @@
   (interactive "sYour GitHub account: ")
   (eww-browse-url (format "https://grass-graph.moshimo.works/images/%s.png" account)))
 
-(defun view-github-page (account)
-  (interactive "sYour GitHub account: ")
-  (eww-browse-url (format "https://github.com/%s" account)))
-
 (defun search-at-qiita (word)
   (interactive "sKeyword: ")
   (eww-browse-url (format "https://qiita.com/search?q=%s" word)))
+
