876c96e2df2c458afc56d26680e82d9555ea287e
ShunjiroOsada <shunjiroosada@gmail.com>
2020-06-16 07:04:31 +0900

    modified some files for output pdf by org-mode

--- conf/org_set.el
+++ conf/org_set.el
@@ -1,67 +1,24 @@
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
+(require 'org)
 (require 'ox-latex)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(setq org-latex-default-class "bxjsarticle")
+(require 'ox-bibtex)
 
-(add-to-list 'org-latex-classes
-             '("koma-article"
-               "\\documentclass{scrartcl}"
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(setq org-latex-classes '(("ltjsarticle"
+            "\\documentclass{ltjsarticle}
+\\usepackage{graphicx}
+\\usepackage{color}
+\\usepackage{atbegshi}
+\\usepackage[unicode=true,bookmarks=true]{hyperref}
+\\usepackage{bookmark}
+\\usepackage{url}
+[NO-DEFAULT-PACKAGES]
+[PACKAGES]
+[EXTRA]"
+            ("\\section{%s}" . "\\section*{%s}")
+            ("\\subsection{%s}" . "\\subsection*{%s}")
+            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+            ("\\paragraph{%s}" . "\\paragraph*{%s}")
+            ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+               ))
 
-(add-to-list 'org-latex-classes
-             '("koma-jarticle"
-               "\\documentclass{scrartcl}
-               \\usepackage{amsmath}
-               \\usepackage{amssymb}
-               \\usepackage{xunicode}
-               \\usepackage{fixltx2e}
-               \\usepackage{zxjatype}
-               \\usepackage[hiragino-dx]{zxjafont}
-               \\usepackage{xltxtra}
-               \\usepackage{graphicx}
-               \\usepackage{longtable}
-               \\usepackage{float}
-               \\usepackage{wrapfig}
-               \\usepackage{soul}
-               \\usepackage{hyperref}"
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
-;; tufte-handout class for writing classy handouts and papers
-(add-to-list 'org-latex-classes
-             '("tufte-handout"
-               "\\documentclass[twoside,nobib]{tufte-handout}
-                                 [NO-DEFAULT-PACKAGES]
-                \\usepackage{zxjatype}
-                \\usepackage[hiragino-dx]{zxjafont}"
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")))
-;; tufte-book class
-(add-to-list 'org-latex-classes
-             '("tufte-book"
-               "\\documentclass[twoside,nobib]{tufte-book}
-                                [NO-DEFAULT-PACKAGES]
-                 \\usepackage{zxjatype}
-                 \\usepackage[hiragino-dx]{zxjafont}"
-               ("\\part{%s}" . "\\part*{%s}")
-               ("\\chapter{%s}" . "\\chapter*{%s}")
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")))
-
-(setq org-latex-pdf-process
-      '("xelatex -interaction nonstopmode -output-directory %o %f"
-        "bibtex %b"
-        "xelatex -interaction nontopmode -output-directory %o %f"
-        "xelatex -interaction nonstopmode -output-directory %o %f"))
+(setq org-latex-default-class "ltjsarticle")
+(setq org-latex-pdf-process '("lualatex %b" "lualatex %b"))
