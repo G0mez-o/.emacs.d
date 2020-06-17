99d08b581aa97cb2557d39f78faf1db4fadc53a8
ShunjiroOsada <shunjiroosada@gmail.com>
2020-06-17 23:54:30 +0900

    modified a org-mode setting file for using reftex

--- conf/org_set.el
+++ conf/org_set.el
@@ -22,3 +22,16 @@
 
 (setq org-latex-default-class "ltjsarticle")
 (setq org-latex-pdf-process '("lualatex %b" "lualatex %b"))
+
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name)
+       (file-exists-p (buffer-file-name))
+       (reftex-parse-all))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
+  )
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
+(setq reftex-external-file-finders
+       '(("tex" . "kpsewhich -format=.tex %f")
+	 ("bib" . "kpsewhich -format=.bib %f")))
+(setq reftex-default-bibliography '("~/org_workspace/clustering.bib"))
